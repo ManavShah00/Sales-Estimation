c6cb15877c87cb2850b89f0ba60ba677048c45a1
ManavShah00 <62293101+ManavShah00@users.noreply.github.com>
2021-04-22 11:18:05 +0530

    Add files via upload

---
+++ Linear_Regression/Sales_estimation.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+import os
+
+data = pd.read_csv("C:/Users/admin/Desktop/Linear_Regression/data/Advertising_data.csv")
+data.head(10)
